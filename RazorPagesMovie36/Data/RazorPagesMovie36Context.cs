9a272c84a0f10e8d246dd6e2c6cee3c54c9ab596
Scot <bscot.hagen@gmail.com>
2021-08-18 10:30:14 -0700

    Added Model, scaffolding, seed data, search, Rating field, data annotations

---
+++ RazorPagesMovie36/Data/RazorPagesMovie36Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie36.Models;
+
+namespace RazorPagesMovie36.Data
+{
+    public class RazorPagesMovie36Context : DbContext
+    {
+        public RazorPagesMovie36Context (DbContextOptions<RazorPagesMovie36Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie36.Models.Movie> Movie { get; set; }
+    }
+}
